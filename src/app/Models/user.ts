9f4471b2bba3866d7d6a8e35f50e1ccfec1fa97e
EDZA09 <eduarstiven2001@gmail.com>
2024-02-21 11:03:52 -0500

    Feat(Models/User): Defino la clase User junto con sus atributos

---
+++ src/app/Models/user.ts
@@ -0,0 +1,11 @@
+export class Uers {
+  constructor(
+    public id: number,
+    public nme: string,
+    public surname: string,
+    public email: string,
+    public password: string,
+    public role: string,
+    public created_at: string
+  ) {}
+}
