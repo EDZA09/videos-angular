14423da64996c595f24d6335d156464947c046d9
EDZA09 <eduarstiven2001@gmail.com>
2024-02-22 11:44:22 -0500

    Build(Routes): Importo los componentes creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ErrorComponent } from './components/error/error.component';
+
 export const routes: Routes = [];
