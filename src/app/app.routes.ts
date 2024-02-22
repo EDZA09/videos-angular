0de08b370ed3967fd7d29e859e1f33288766ff33
EDZA09 <eduarstiven2001@gmail.com>
2024-02-22 12:03:08 -0500

    Feat(Routes): Defino las rutas de los componentes home, register, login y error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,11 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/inicio' },
+  { path: 'inicio', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: '/error' },
+];
