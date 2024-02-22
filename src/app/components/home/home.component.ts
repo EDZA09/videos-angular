aa0920e1ece9dfeb376f18b4d7d52af08294e0aa
EDZA09 <eduarstiven2001@gmail.com>
2024-02-22 10:58:52 -0500

    Feat(Components/home): Creo la propiedad page_title para mostrarla en la vista

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  public page_title: string;
 
+  constructor() {
+    this.page_title = 'Inicio';
+  }
 }
