88d443f3d3234cb5808dd5ecab8c4f98db2a4ff0
EDZA09 <eduarstiven2001@gmail.com>
2024-02-22 11:03:07 -0500

    Feat(Components/register): Creo la propiedad page_title para mostrarla en la vista

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  public page_title: string;
 
+  constructor() {
+    this.page_title = 'Registro';
+  }
 }
