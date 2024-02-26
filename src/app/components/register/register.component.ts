b0cd60ecc6af1af83c5c2c489aec373bcf75e524
EDZA09 <eduarstiven2001@gmail.com>
2024-02-26 09:45:14 -0500

    Feat(components/register): Importo el FormsModule y creo una función que recibe los datos del formulario

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
 import { User } from '../../Models/user';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -15,5 +18,9 @@ export class RegisterComponent {
   constructor() {
     this.page_title = 'Registro';
     this.user = new User(1, '', '', '', '', 'ROLE_USER', '');
+  }
+
+  onSubmit(formValue) {
+    console.log(formValue);
   }
 }
