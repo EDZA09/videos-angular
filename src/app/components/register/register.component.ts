2523722f2c8aa33b31d8b304ed58b324981ceeab
EDZA09 <eduarstiven2001@gmail.com>
2024-02-28 11:27:39 -0500

    Feat(component/register): Utilizo el método register del UserService y valido el resultado

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -12,17 +12,27 @@ import { UserService } from '../../services/user.service';
   imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
+  providers: [UserService],
 })
 export class RegisterComponent {
   public page_title: string;
   public user: User;
+  public status: string;
 
   constructor(private _userService: UserService) {
     this.page_title = 'Registro';
     this.user = new User(1, '', '', '', '', 'ROLE_USER', '');
+    this.status = '';
   }
 
   onSubmit(form: any) {
-    console.log(this.user);
+    this._userService.register(this.user).subscribe((response) => {
+      if (response.status == 'success') {
+        this.status = 'success';
+        form.reset();
+      } else {
+        this.status = 'error';
+      }
+    });
   }
 }
