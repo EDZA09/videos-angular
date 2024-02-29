0fa0228f355e271ea1f0ee94abe4ed600e407009
EDZA09 <eduarstiven2001@gmail.com>
2024-02-29 09:44:47 -0500

    Feat(components/login): Importo los modulos, modelos y servicios necesarios para la lógica

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { User } from '../../Models/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  providers: [UserService],
 })
 export class LoginComponent {
   public page_title: string;
