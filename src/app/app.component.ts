04d3b30a97ebf9635a690186856b5b2868f71f02
EDZA09 <eduarstiven2001@gmail.com>
2024-03-01 12:17:55 -0500

    Feat(components/app): Importo e Inyecto como depencia el servicio de usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
+import { UserService } from './services/user.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [UserService],
 })
 export class AppComponent {
   title = 'Aplicación de videos favoritos';
+
+  constructor(private _userService: UserService) {}
 }
