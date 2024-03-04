ec200864fe4733cb9ac748c1667c23cc59bc0b60
EDZA09 <eduarstiven2001@gmail.com>
2024-03-03 20:23:52 -0500

    Feat(components): Creo los componentes user-edit y video-new

---
+++ src/app/components/video-new/video-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-new.component.html',
+  styleUrl: './video-new.component.css'
+})
+export class VideoNewComponent {
+
+}
