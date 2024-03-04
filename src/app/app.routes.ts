2d28ddf7b17695ab756f6ca462caa1659c83c407
EDZA09 <eduarstiven2001@gmail.com>
2024-03-03 20:32:30 -0500

    Feat(Components/video-new): Creo la ruta del componente y video-new la anexo en la vista principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ErrorComponent } from './components/error/error.component';
+import { VideoNewComponent } from './components/video-new/video-new.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout/:sure', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'guardar-favorito', component: VideoNewComponent },
   { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: '/error', pathMatch: 'full' },
 ];
