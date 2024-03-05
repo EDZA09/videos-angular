026497942e00e75724ed471d327d381c0cafecbc
EDZA09 <eduarstiven2001@gmail.com>
2024-03-05 10:32:39 -0500

    Feat(components/home): Creo la propiedad token

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,9 +14,11 @@ import { UserService } from '../../services/user.service';
 export class HomeComponent {
   public page_title: string;
   public identity: any;
+  public token: string;
 
   constructor(private _userService: UserService) {
     this.page_title = 'Inicio';
     this.identity = this._userService.getIdentity();
+    this.token = '';
   }
 }
