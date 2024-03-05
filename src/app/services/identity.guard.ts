6a5ff4b3f4260b4ce3a4bf0d75c491885d73a976
EDZA09 <eduarstiven2001@gmail.com>
2024-03-05 09:55:03 -0500

    Feat(services/identity.guard): Utilizo el método CanActivate y obtengo el item 'Identity'

--- src/app/services/identity.guard.ts
+++ src/app/services/identity.guard.ts
@@ -5,4 +5,8 @@ import { UserService } from './user.service';
 @Injectable()
 export class IdentityGuard implements CanActivate {
   constructor(private _router: Router, private _userService: UserService) {}
+
+  canActivate() {
+    let identity = this._userService.getIdentity();
+  }
 }
