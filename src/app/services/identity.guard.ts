5dd87441503074d060eec055ced66901ec114b4d
EDZA09 <eduarstiven2001@gmail.com>
2024-03-05 09:53:11 -0500

    Feat(services/identity.guard): Implemento CanActivate e inyecto las dependencias Router y UserService

--- src/app/services/identity.guard.ts
+++ src/app/services/identity.guard.ts
@@ -1,3 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { UserService } from './user.service';
+
+@Injectable()
+export class IdentityGuard implements CanActivate {
+  constructor(private _router: Router, private _userService: UserService) {}
+}
